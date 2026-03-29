21001020c8b64fce7eff7d8810a02522b68450b1
Emmanuel-Praise <epraise527@gmail.com>
2026-03-29 08:00:39 +0100

    Add temporary database fix route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Temporary Database Fix Route
+Route::get('/fix-database', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        return 'Migrations successful! <br><pre>' . \Illuminate\Support\Facades\Artisan::output() . '</pre>';
+    } catch (\Exception $e) {
+        return 'Error running migrations: ' . $e->getMessage();
+    }
+});
